db8a2820f6a4c2e805d079ab57f712e377283b88
Rob Taylor <manix84@gmail.com>
2012-06-10 13:15:21 +0100

    Updating bootstrap to be aware of hosts.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,8 @@
 cd "$(dirname "$0")"
 git pull
 function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
+	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" --exclude "hosts" -av . ~
+    sudo cp -f hosts /private/etc/
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
