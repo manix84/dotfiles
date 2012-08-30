b2ae13872b546d751227b35cce2f162bbffd9afa
Taylor <robtay@406c8f08ddaf.ant.amazon.com>
2012-08-30 16:42:09 +0100

    Adding HOSTS file installation check, as I don't really want this at work

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 git pull
+
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" --exclude "hosts" -av . ~
-    sudo cp -f hosts /private/etc/
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
@@ -15,4 +15,20 @@ else
 	fi
 fi
 unset doIt
+
 source ~/.bash_profile
+
+
+function installHosts() {
+    sudo cp -f hosts /private/etc/
+}
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+    installHosts
+else
+    read -p "Do you wish to also copy the HOSTS file? This will require your password as it will be sudo'd into place (y/n) " -n 1
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        installHosts
+    fi
+fi
+unset installHosts
