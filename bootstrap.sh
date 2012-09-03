9f95873e2ecd37622061347aa8d791f7e96847bd
Taylor <robtay@406c8f08ddaf.ant.amazon.com>
2012-09-03 16:16:47 +0100

    Cleaning up hosts file message.

--- bootstrap.sh
+++ bootstrap.sh
@@ -31,7 +31,7 @@ function installHosts() {
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
     installHosts
 else
-    read -p "Do you wish to also copy the HOSTS file? This will require your password as it will be sudo'd into place (y/n) " -n 1
+    read -p "Do you wish to also copy the HOSTS file? (y/n) " -n 1
     echo
     if [[ $REPLY =~ ^[Yy]$ ]]; then
         installHosts
