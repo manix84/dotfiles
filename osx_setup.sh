5fcaa008294f484bcbe1c4ddbe13074c79914af0
Rob Taylor <manix84@gmail.com>
2017-07-05 14:36:36 +0100

    Adding iTerm2 configuration pointers.

--- osx_setup.sh
+++ osx_setup.sh
@@ -498,6 +498,15 @@ defaults write com.twitter.twitter-mac ShowFullNames -bool true
 defaults write com.twitter.twitter-mac HideInBackground -bool true
 
 ###############################################################################
+# iTerm2                                                                      #
+###############################################################################
+
+# Specify the preferences directory
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.iterm2"
+# Tell iTerm2 to use the custom preferences in the directory
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+
+###############################################################################
 # Kill affected applications                                                  #
 ###############################################################################
 
